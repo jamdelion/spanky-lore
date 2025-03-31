1d26d1ea5e0c0e9120a6dd6c1a314f4600a3b6af
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2025-03-31 23:06:57 +0100

    Start a lore array

---
+++ lore.js
@@ -0,0 +1,14 @@
+export const lore = [
+  {
+    date: "Oct 2012",
+    event: "Fingal joins the band",
+    details:
+      "Fingal Plumpton joins Spankys on tenor sax, in his first year at Clare College",
+  },
+  {
+    date: "Oct 2013",
+    event: "Jo joins the band",
+    details:
+      "Jo Humphrey joins Spankys as the new drummer, in their second year at Clare College",
+  },
+];
